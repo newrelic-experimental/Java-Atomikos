fd4fbcdf8a94c6373d44939bd5d900cff230fdab
gsidhwani_nr <gsidhwani@newrelic.com>
2024-07-01 11:53:45 +0530

    feat: Added Support for 6.0

---
+++ atomikos-cp-6.0/src/main/java/com/atomikos/jdbc/internal/AbstractDataSourceBean.java
@@ -0,0 +1,48 @@
+package com.atomikos.jdbc.internal;
+
+import java.sql.Connection;
+
+import com.atomikos.datasource.pool.ConnectionFactory;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.atomikos.jdbc.DataSourceSampler;
+
+@Weave(type=MatchType.BaseClass)
+public abstract class AbstractDataSourceBean {
+	
+	@NewField
+	protected boolean initialized = false;
+	
+	public abstract String getUniqueResourceName();
+	
+	public abstract int poolAvailableSize();
+
+	public abstract int poolTotalSize();
+	
+	protected ConnectionFactory doInit() {
+		if(!DataSourceSampler.getInstance().contains(this)) {
+			DataSourceSampler.getInstance().addPool(this);
+		}
+		return Weaver.callOriginal();
+	}
+
+	protected void doClose() {
+		if(DataSourceSampler.getInstance().contains(this)) {
+			DataSourceSampler.getInstance().removePool(this);
+		}
+
+		Weaver.callOriginal();
+	}
+
+	public Connection getConnection() {
+		if(!initialized) {
+			if(!DataSourceSampler.getInstance().contains(this)) {
+				DataSourceSampler.getInstance().addPool(this);
+			}
+			initialized = true;
+		}
+		return Weaver.callOriginal();
+	}
+}
